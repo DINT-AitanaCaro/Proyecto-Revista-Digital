9730182fed81eb9dd0f7a3d8293cd77eb4024d8c
AitanaCaro <aitana.caro@iesdoctorbalmis.com>
2023-01-24 09:47:47 +0100

    ServicioSQLite métodos crear, editar, eliminar en bd implementados

--- Proyecto-Revista-Digital/Servicios/ServicioSQLite.cs
+++ Proyecto-Revista-Digital/Servicios/ServicioSQLite.cs
@@ -17,7 +17,8 @@ namespace Proyecto_Revista_Digital.Servicios
             conexion.Open();
             SqliteCommand comando = conexion.CreateCommand();
             comando.CommandText = @"CREATE TABLE IF NOT EXISTS autores (
-                  nombre varchar(100),                      
+                  id INTEGER primary key
+                  nombre varchar(100) NOT NULL,                      
                   imagen varchar(100),                      
                   nickname varchar(50),  
                   social varchar(50) CHECK( social IN ('Instagram','Twitter','Facebook') ) 
@@ -25,7 +26,7 @@ namespace Proyecto_Revista_Digital.Servicios
             comando.ExecuteNonQuery();
 
             comando.CommandText = @"CREATE TABLE IF NOT EXISTS articulos (
-                  titulo varchar(100),                      
+                  titulo varchar(100) primary key,                      
                   imagen varchar(100),                      
                   contenido varchar(50),  
                                     )";
@@ -45,6 +46,42 @@ namespace Proyecto_Revista_Digital.Servicios
             comando.Parameters["@imagen"].Value = autor.Imagen;
             comando.Parameters["@nickname"].Value = autor.Nickname;
             comando.Parameters["@social"].Value = autor.Social;
+
+            comando.ExecuteNonQuery();
+            conexion.Close();
+        }
+
+        public void EditAutor(Autor autor)
+        {   
+            conexion.Open();
+            SqliteCommand comando = conexion.CreateCommand();
+            comando.CommandText = "UPDATE autores SET nombre = @nombre, imagen = @imagen, nickname = @nickname, social = @social WHERE id = @id";
+
+            comando.Parameters.Add("@nombre", SqliteType.Text);
+            comando.Parameters.Add("@imagen", SqliteType.Text);
+            comando.Parameters.Add("@nickname", SqliteType.Text);
+            comando.Parameters.Add("@social", SqliteType.Text);
+            comando.Parameters.Add("@id", SqliteType.Integer);
+
+            comando.Parameters["@nombre"].Value = autor.Nombre;
+            comando.Parameters["@imagen"].Value = autor.Imagen;
+            comando.Parameters["@nickname"].Value = autor.Nickname;
+            comando.Parameters["@social"].Value = autor.Social;
+            comando.Parameters["@id"].Value = autor.Id;
+
+            comando.ExecuteNonQuery();
+            conexion.Close();
+        }
+
+        public void DeleteAutor(int autorId)
+        {
+            conexion.Open();
+            SqliteCommand comando = conexion.CreateCommand();
+            comando.CommandText = "DELETE FROM autores WHERE id = @id";
+
+            comando.Parameters.Add("@id", SqliteType.Integer);
+
+            comando.Parameters["@id"].Value = autorId;
 
             comando.ExecuteNonQuery();
             conexion.Close();
