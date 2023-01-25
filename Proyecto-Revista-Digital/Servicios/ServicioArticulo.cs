ec556c3abf0df20bbb73fe365d7acf0bbccaae28
Aitana Caro Martínez <aitana.caro@iesdoctorbalmis.com>
2023-01-25 21:09:59 +0100

    servicio articulo no implementado

---
+++ Proyecto-Revista-Digital/Servicios/ServicioArticulo.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.Data.Sqlite;
+using Proyecto_Revista_Digital.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_Revista_Digital.Servicios
+{
+    class ServicioArticulo
+    {
+        private SqliteConnection conexion = new SqliteConnection("Data Source=revista.db");
+        public void AddArticulo(Articulo articulo)
+        {
+            conexion.Open();
+            SqliteCommand comando = conexion.CreateCommand();
+            comando.CommandText = "INSERT INTO articulos VALUES(@titulo,@idAutor,@seccion,@imagen,@contenido,@publicado)";
+            comando.Parameters.Add("@titulo", SqliteType.Text);
+            comando.Parameters.Add("@idAutor", SqliteType.Integer);
+            comando.Parameters.Add("@seccion", SqliteType.Text);
+            comando.Parameters.Add("@imagen", SqliteType.Text);
+            comando.Parameters.Add("@contenido", SqliteType.Text);
+            comando.Parameters.Add("@publicado", SqliteType.Integer);
+
+            comando.Parameters["@titulo"].Value = articulo.Titulo;
+            comando.Parameters["@idAutor"].Value = articulo.IdAutor;// o Autor.Id al pasar a Articulo hay q consultar al autor de ese id para parsearlo
+            comando.Parameters["@seccion"].Value = articulo.Seccion;
+            comando.Parameters["@imagen"].Value = articulo.Imagen;
+            comando.Parameters["@contenido"].Value = articulo.Contenido;
+            comando.Parameters["@publicado"].Value = articulo.Publicado;
+
+            comando.ExecuteNonQuery();
+            conexion.Close();
+        }
+
+        public void EditArticulo(Articulo articulo)
+        {
+            conexion.Open();
+            SqliteCommand comando = conexion.CreateCommand();
+            comando.CommandText = "UPDATE articulos SET titulo = @titulo, idAutor = @idAutor, seccion = @seccion, contenido  = @contenido, publicado = @publicado WHERE id = @id";
+            /*
+            comando.Parameters.Add("@titulo", SqliteType.Text);
+            comando.Parameters.Add("@idAutor", SqliteType.Integer);
+            comando.Parameters.Add("@seccion", SqliteType.Integer);
+            comando.Parameters.Add("@imagen", SqliteType.Text);
+            comando.Parameters.Add("@contenido", SqliteType.Text);
+            comando.Parameters.Add("@publicado", SqliteType.Integer);
+
+            comando.Parameters["@titulo"].Value = articulo.Titulo;
+            comando.Parameters["@idAutor"].Value = articulo.IdAutor;
+            comando.Parameters["@seccion"].Value = articulo.Seccion;
+            comando.Parameters["@imagen"].Value = articulo.Imagen;
+            comando.Parameters["@contenido"].Value = articulo.Contenido;
+            comando.Parameters["@publicado"].Value = articulo.Publicado;
+            */
+            comando.ExecuteNonQuery();
+            conexion.Close();
+        }
+
+        public void DeleteArticulo(int articuloId)
+        {
+            conexion.Open();
+            SqliteCommand comando = conexion.CreateCommand();
+            comando.CommandText = "DELETE FROM articulos WHERE id = @id";
+
+            comando.Parameters.Add("@id", SqliteType.Integer);
+
+            comando.Parameters["@id"].Value = articuloId;
+
+            comando.ExecuteNonQuery();
+            conexion.Close();
+        }
+        /*
+        public ObservableCollection<Articulo> GetArticulos()
+        {
+            conexion.Open();
+            SqliteCommand comando = conexion.CreateCommand();
+            comando.CommandText = "SELECT * FROM articulos";
+            ObservableCollection<Articulo> articulos = new ObservableCollection<Articulo>();
+            SqliteDataReader lector = comando.ExecuteReader();
+            if(lector.HasRows)
+            {
+                while (lector.Read())
+                {
+                    articulos.Add(new Articulo(Convert.ToInt32(lector["titulo"]), (string)lector["i"], (string)lector["imagen"], (string)lector["nickname"], (string)lector["social"]));
+                }
+            }
+            lector.Close();
+            conexion.Close();
+            return autores;
+        }*/
+    }
+}
--- Proyecto-Revista-Digital/Servicios/ServicioSQLite.cs
+++ Proyecto-Revista-Digital/Servicios/ServicioSQLite.cs
@@ -17,19 +17,29 @@ namespace Proyecto_Revista_Digital.Servicios
             conexion.Open();
             SqliteCommand comando = conexion.CreateCommand();
             comando.CommandText = @"CREATE TABLE IF NOT EXISTS autores (
-                  id INTEGER primary key
-                  nombre varchar(100) NOT NULL,                      
-                  imagen varchar(100),                      
-                  nickname varchar(50),  
-                  social varchar(50) CHECK( social IN ('Instagram','Twitter','Facebook') ) 
+                    id INTEGER primary key
+                    nombre varchar(100) NOT NULL,                      
+                    imagen varchar(100),                      
+                    nickname varchar(50),  
+                    social varchar(50) CHECK( social IN ('Instagram','Twitter','Facebook') ) 
                                     )";
             comando.ExecuteNonQuery();
 
             comando.CommandText = @"CREATE TABLE IF NOT EXISTS articulos (
-                  titulo varchar(100) primary key,                      
-                  imagen varchar(100),                      
-                  contenido varchar(50),  
+                    id INTEGER,
+                    titulo varchar(100) primary key,
+                    idAutor INTEGER,
+                    seccion varchar(50),
+                    imagen varchar(100),                      
+                    contenido varchar(50), 
+                    publicado INTEGER,
+                    FOREIGN KEY (idAutor) REFERENCES autores(id),
+                    PRIMARY KEY(id, titulo)
                                     )";
+            /*
+            comando.CommandText = @"CREATE TABLE IF NOT EXISTS secciones (
+                    titulo varchar(100) primary key,
+                                    )";*/
             conexion.Close();
         }
     }
