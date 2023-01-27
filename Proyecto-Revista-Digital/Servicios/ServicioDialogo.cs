84cdc53f1c1d3582a5bfbd73e5263f5395ddc517
Aitana Caro Martínez <aitana.caro@iesdoctorbalmis.com>
2023-01-27 19:00:03 +0100

    ServicioDialogo (no terminado)

---
+++ Proyecto-Revista-Digital/Servicios/ServicioDialogo.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_Revista_Digital.Servicios
+{
+    class ServicioDialogo
+    {
+        public string DialogoAbrirFichero()
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Image files (*.png;*.jpeg)|*.png;*.jpeg|All files (*.*)|*.*";
+            return openFileDialog.ShowDialog() == true ? openFileDialog.FileName : null;
+        }
+    }
+}
