e86e7bcae57241274876d85f46e91af79ccd8144
Jose Alfredo Zaragoza Marco <joshalfre10@gmail.com>
2023-01-28 22:22:19 +0100

    Creacion ventana GestionArticulos.xaml y GestionArticulosVM.cs

--- Proyecto-Revista-Digital/Vistas/GestionArticulos.xaml.cs
+++ Proyecto-Revista-Digital/Vistas/GestionArticulos.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Proyecto_Revista_Digital.VistasModelo;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +21,12 @@ namespace Proyecto_Revista_Digital.Vistas
     /// </summary>
     public partial class GestionArticulos : UserControl
     {
+        private GestorArticulosVM vm;
         public GestionArticulos()
         {
             InitializeComponent();
+            vm = new GestorArticulosVM();
+            this.DataContext = vm;
         }
     }
 }
--- Proyecto-Revista-Digital/VistasModelo/GestorArticulosVM.cs
+++ Proyecto-Revista-Digital/VistasModelo/GestorArticulosVM.cs
@@ -1,13 +1,37 @@
-﻿using System;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using Proyecto_Revista_Digital.Modelos;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Proyecto_Revista_Digital.VistasModelo
 {
-    class GestorArticulosVM
+    class GestorArticulosVM : ObservableObject
     {
+		private ObservableCollection<Articulo> articulos;
 
-    }
+		public ObservableCollection<Articulo> Articulos
+        {
+			get { return articulos; }
+			set { SetProperty(ref articulos, value); }
+		}
+
+        private Articulo articuloSeleccionado;
+
+        public Articulo ArticuloSeleccionado
+        {
+            get { return articuloSeleccionado; }
+            set { SetProperty(ref articuloSeleccionado, value); }
+        }
+
+        public GestorArticulosVM()
+		{
+			Articulos = new ObservableCollection<Articulo>();
+            ArticuloSeleccionado = new Articulo();
+		}
+
+	}
 }
