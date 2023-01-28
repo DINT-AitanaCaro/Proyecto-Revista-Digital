64e5d1ae638d7b7d3ea828aeec00ede5f1823dcc
IAN ESTEBAN TAUZY LAMAGNA <iantaulam@alu.edu.gva.es>
2023-01-28 21:36:50 +0100

    Avance CrearArticulo - Arreglar chapuza

--- Proyecto-Revista-Digital/VistasModelo/CrearArticuloVM.cs
+++ Proyecto-Revista-Digital/VistasModelo/CrearArticuloVM.cs
@@ -1,6 +1,7 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using Proyecto_Revista_Digital.Modelos;
+using Proyecto_Revista_Digital.Servicios;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
@@ -37,9 +38,23 @@ namespace Proyecto_Revista_Digital.VistasModelo
             set { SetProperty(ref listaAutores, value); }
         }
 
+        private ObservableCollection<string> nombreAutores;
+
+        public ObservableCollection<string> NombreAutores
+        {
+            get { return nombreAutores; }
+            set { SetProperty(ref nombreAutores, value); }
+        }
+
+
         public RelayCommand NuevaSeccionCommand { get; }
         public RelayCommand NuevoArticuloCommand { get; }
         public RelayCommand NuevaImagenArticuloCommand { get; }
+        private ServicioArticulo servicoArticulo;
+        private ServicioAutor servicioAutor;
+        private ServicioSeccion servicioSeccion;
+        private ServicioDialogo servicioDialogo;
+        private ServicioAzure servicioAzure;
 
         /*private Seccion seccionArticulo;
 
@@ -59,11 +74,41 @@ namespace Proyecto_Revista_Digital.VistasModelo
 
         public CrearArticuloVM()
         {
+            NombreAutores = new ObservableCollection<string>();
             ArticuloNuevo = new Articulo();
             ListaSecciones = new ObservableCollection<Seccion>();
             ListaAutores = new ObservableCollection<Autor>();
+            servicioAutor = new ServicioAutor();
+            servicioAzure = new ServicioAzure();
+            servicioDialogo = new ServicioDialogo();
+            servicioSeccion = new ServicioSeccion();
+            servicoArticulo = new ServicioArticulo();
+            NuevaImagenArticuloCommand = new RelayCommand(SeleccionImagen);
+            CargarAutores();
+            //CargarSecciones();
             //SeccionArticulo = new Seccion();
             //AutorArticulo = new Autor();
+        }
+
+        private void CargarAutores()
+        {
+            ListaAutores = servicioAutor.GetAutores();
+
+            foreach (Autor item in ListaAutores)
+            {
+                NombreAutores.Add(item.Nombre);
+            }
+        }
+
+        private void CargarSecciones()
+        {
+            ListaSecciones = servicioSeccion.GetSecciones();
+        }
+
+        public void SeleccionImagen()
+        {
+            string file = servicioDialogo.DialogoAbrirFichero();
+            ArticuloNuevo.Imagen = file != null ? servicioAzure.AlmacenarImagenEnLaNube(file) : string.Empty;
         }
     }
 }
