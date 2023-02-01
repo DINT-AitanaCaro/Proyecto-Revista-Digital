ecb236771fd6baf194f2786cfa35142d56118b49
AitanaCaro <aitana.caro@iesdoctorbalmis.com>
2023-02-01 10:10:37 +0100

    Modelo lista terminos implementada

--- Proyecto-Revista-Digital/Modelos/ListaTerminos.cs
+++ Proyecto-Revista-Digital/Modelos/ListaTerminos.cs
@@ -1,12 +1,53 @@
-﻿using System;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Proyecto_Revista_Digital.Modelos
 {
-    class ListaTerminos
+    class ListaTerminos : ObservableObject
     {
+        private int id;
+        public int Id
+        {
+            get { return id; }
+            set { SetProperty(ref id, value); }
+        }
+
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set { SetProperty(ref name, value); }
+        }
+        private string description;
+
+        public string Description
+        {
+            get { return description; }
+            set { SetProperty(ref description, value); }
+        }
+
+        private ObservableCollection<string> _terms;
+        public ObservableCollection<string> Terms
+        {
+            get { return _terms; }
+            set { SetProperty(ref _terms, value); }
+        }
+
+        public ListaTerminos(string name, string description)
+        {
+            Name = name;
+            Description = description;
+        }
+
+        public override string ToString()
+        {
+            return $"Id: {Id}. Name: {Name}, Description: {Description}.";
+        }
     }
 }
