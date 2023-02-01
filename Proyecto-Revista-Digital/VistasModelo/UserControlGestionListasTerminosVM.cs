70a0acde8e8f19e4cdf683e2de27a94588532ffd
AitanaCaro <aitana.caro@iesdoctorbalmis.com>
2023-02-01 10:15:53 +0100

    Creado servicio ServicioAPIRestListasTerminos (vacío)

--- Proyecto-Revista-Digital/VistasModelo/UserControlGestionListasTerminosVM.cs
+++ Proyecto-Revista-Digital/VistasModelo/UserControlGestionListasTerminosVM.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CommunityToolkit.Mvvm.Input;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,6 @@ namespace Proyecto_Revista_Digital.VistasModelo
 {
     class UserControlGestionListasTerminosVM
     {
+        public RelayCommand AddListaCommand { get; }
     }
 }
